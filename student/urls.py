42a3a319a1d3aae76c20b0d3a0c50e8059608e2c
agentauthorise <agentauthorise@gmail.com>
2018-01-03 08:40:36 +0800

    data dh ok

--- student/urls.py
+++ student/urls.py
@@ -2,7 +2,7 @@ from django.conf.urls import include,url
 from . import views
 
 urlpatterns = [
-	url(r'^$',views.home,name='student_home'),
-	url(r'^$home/$',views.home_json,name='student_home_json'),
-	url(r'^list_json/$',views.student_list_json.as_view(),name="student_list_json"),
-	]	
+    url(r'^$',views.home,name='student_home'),
+    url(r'^home/$',views.home_json,name='student_home_json'),
+    url(r'^list_json/$',(views.student_list_json.as_view()), name="student_list_json"),
+]
--- student/views.py
+++ student/views.py
@@ -9,13 +9,16 @@ from django.contrib.auth.decorators import login_required
 from django_datatables_view.base_datatable_view import BaseDatatableView
 from django.db.models import Count, Sum, Q, Case, Value, When, IntegerField
 
+
 # Create your views here.
 
 def home(request):
-	return render(request,'base.html')
+		return render(request,'base.html')
 
 def home_json(request):
-	return render(request,'student/home_json.html')
+    return render(request, 'student/home_json.html')
+
+		
 
 # Student JSON list filtering
 class student_list_json(BaseDatatableView):
@@ -91,7 +94,8 @@ class student_list_json(BaseDatatableView):
                 # item.course,
                 item.get_course_display(),
                 str(item.pk),
+                #reverse_lazy('student_detail',kwargs={'pk': str(item.pk)})
                 reverse_lazy('student_home'),
-            ])    
+            ])
             # print(json_data)
         return json_data
